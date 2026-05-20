03c517a11627f0c9124d1dcd423bc29c94d3c207
autoresearch-gpu7 <tangshua-swarm@amazon.com>
2026-05-20 23:01:36 +0000

    may19-gpu1: try smaller init scale s=sqrt(2)/sqrt(n) instead of sqrt(3)/sqrt(n)

--- train.py
+++ train.py
@@ -156,7 +156,7 @@ class GPT(nn.Module):
         torch.nn.init.normal_(self.lm_head.weight, mean=0.0, std=0.001)
         # Transformer blocks
         n_embd = self.config.n_embd
-        s = 3**0.5 * n_embd**-0.5
+        s = 2**0.5 * n_embd**-0.5
         for block in self.transformer.h:
             torch.nn.init.uniform_(block.attn.c_q.weight, -s, s)
             torch.nn.init.uniform_(block.attn.c_k.weight, -s, s)
