bec10f3eb5b21f5b0207e5f4168bc16ab782a23a
autoresearch-gpu7 <tangshua-swarm@amazon.com>
2026-05-20 19:10:53 +0000

    may19-gpu3: add fullgraph=True to model compile

--- train.py
+++ train.py
@@ -509,7 +509,7 @@ optimizer = model.setup_optimizer(
     weight_decay=WEIGHT_DECAY,
 )
 
-model = torch.compile(model, dynamic=False, mode="max-autotune")
+model = torch.compile(model, dynamic=False, mode="max-autotune", fullgraph=True)
 
 train_loader = make_dataloader(tokenizer, DEVICE_BATCH_SIZE, MAX_SEQ_LEN, "train")
 x, y, epoch = next(train_loader)  # prefetch first batch
