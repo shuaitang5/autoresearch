15a6adc16b509803020cabec6d524c7f965d6149
autoresearch-gpu7 <tangshua-swarm@amazon.com>
2026-05-20 13:41:36 +0000

    may19-gpu3: MLP expansion 5.5x->6x

--- train.py
+++ train.py
@@ -102,7 +102,7 @@ class CausalSelfAttention(nn.Module):
 class MLP(nn.Module):
     def __init__(self, config):
         super().__init__()
-        hidden = int(5.5 * config.n_embd)
+        hidden = int(6.0 * config.n_embd)
         self.c_fc = nn.Linear(config.n_embd, hidden, bias=False)
         self.c_proj = nn.Linear(hidden, config.n_embd, bias=False)
 
