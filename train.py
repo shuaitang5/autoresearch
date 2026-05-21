afed1d883f95578361137cc78483e1b48d87e57f
autoresearch-gpu7 <tangshua-swarm@amazon.com>
2026-05-21 17:30:31 +0000

    may21-gpu7: final LR frac 0.01 (from 0.02)

--- train.py
+++ train.py
@@ -457,7 +457,7 @@ WEIGHT_DECAY = 0.2      # cautious weight decay for Muon
 ADAM_BETAS = (0.8, 0.95) # Adam beta1, beta2
 WARMUP_RATIO = 0.01      # fraction of time budget for LR warmup
 WARMDOWN_RATIO = 0.95    # fraction of time budget for LR warmdown
-FINAL_LR_FRAC = 0.02     # final LR as fraction of initial
+FINAL_LR_FRAC = 0.01     # final LR as fraction of initial
 
 # Model size
 DEPTH = 9               # number of transformer layers
