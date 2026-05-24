61af033c54a3ea40f26372205fea72e8e6b5f7ce
autoresearch-gpu7 <tangshua-swarm@amazon.com>
2026-05-24 01:00:30 +0000

    may22-gpu6: reduce first layer window from 128 to 64 tokens

--- train.py
+++ train.py
@@ -213,6 +213,7 @@ class GPT(nn.Module):
         window_sizes[-1] = (-1, -1)
         window_sizes[config.n_layer // 2] = (768, 0)
         window_sizes[config.n_layer // 4] = (256, 0)
+        window_sizes[0] = (64, 0)
         return window_sizes
 
     def estimate_flops(self):
