11eec58bfbeabb8c953379f35165d225f72793aa
autoresearch-gpu7 <tangshua-swarm@amazon.com>
2026-05-24 00:00:56 +0000

    [may22-gpu2] experiment: VE on ALL layers (not alternating)

--- train.py
+++ train.py
@@ -49,7 +49,7 @@ def norm(x):
 
 def has_ve(layer_idx, n_layer):
     """Returns True if layer should have Value Embedding (alternating, last always included)."""
-    return layer_idx % 2 == (n_layer - 1) % 2
+    return True
 
 
 def apply_rotary_emb(x, cos, sin):
